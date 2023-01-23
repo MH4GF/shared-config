36e7260a07aeb048c986f241972c26db332ea64d
MH4GF <h.miyagi.cnw@gmail.com>
2023-01-24 08:36:44 +0900

    feat(eslint): setup @typescript-eslint and plugin-import

--- typescript/packages/eslint-config/src/index.js
+++ typescript/packages/eslint-config/src/index.js
@@ -1,8 +1,15 @@
 /** @type {import('eslint/lib/shared/types').ConfigData} */
 const config = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: ['eslint:recommended'],
   overrides: [
     {
       files: '*.{,c,m}{j,t}s{,x}',
+      extends: ['plugin:import/recommended'],
+      plugins: ['@typescript-eslint'],
       rules: {
         'import/order': [
           'error',
